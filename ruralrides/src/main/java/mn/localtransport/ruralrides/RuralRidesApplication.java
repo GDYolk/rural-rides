c07b2c61b9da6fb30345e52fc045f6d7eb3da233
GDYolk <eolk1230@gmail.com>
2024-10-17 20:57:37 +0800

    Initial project setup

---
+++ ruralrides/src/main/java/mn/localtransport/ruralrides/RuralRidesApplication.java
@@ -0,0 +1,13 @@
+package mn.localtransport.ruralrides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuralRidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuralRidesApplication.class, args);
+	}
+
+}
---
+++ ruralrides/src/test/java/mn/localtransport/ruralrides/RuralRidesApplicationTests.java
@@ -0,0 +1,13 @@
+package mn.localtransport.ruralrides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralRidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
